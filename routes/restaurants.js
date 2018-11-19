ae98d4ca85a40efa9e2f63e09b5caacf10a8a35e
Rohnit Shetty <rshetty6@stevens.edu>
2018-11-19 16:44:48 -0500

    Added username to comments

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -5,28 +5,34 @@ const commentsData = require("../data/comments");
 const userData = require("../data/users");
 
 
-router.get("/:id",async (req,res) => {
+router.get("/:id", async (req, res) => {
     try {
         restaurantId = req.params.id;
         restaurant = await restaurantData.getRestaurantById(restaurantId);
         comments = await commentsData.getCommentsByRestaurant(restaurantId);
+        //console.log(comments);
+
+        for(let i = 0; i< comments.length; i++){
+            let user = await userData.getUserById(comments[i].userid)
+            comments[i].username = user.firstname;
+        }
+        //console.log(comments);
         // Object.keys(comments).map(
-        //     async function(object){
+        //     async function (object) {
         //         userModel = await userData.getUserById(comments[object]['userid']);
-        //         console.log("User First name is: " + userModel.firstname);
         //         comments[object]["username"] = userModel.firstname;
-        // });
+        //     });
 
         // comments.forEach( async function(obj) {
         //     userModel = await userData.getUserById(obj.userid);
         //     obj['username'] = userModel.firstname;
         // });
-        
-        // setTimeout(function(){ console.log(comments); }, 3000);
-        // console.log(comments);
-        
+
+        //setTimeout(function(){ console.log(comments); }, 3000);
+        console.log(comments);
+
         res.render('resturants/restaurantDescriptionPage', {
-            restaurant : restaurant,
+            restaurant: restaurant,
             comments: comments
         });
     } catch (error) {
@@ -34,13 +40,13 @@ router.get("/:id",async (req,res) => {
     }
 });
 
-router.post('/addrestaurant', async function(req, res, next) {
+router.post('/addrestaurant', async function (req, res, next) {
     try {
         const restaurantInfo = req.body;
         const restaurant = await restaurantData.addRestaurant(restaurantInfo);
         res.json(restaurant);
     } catch (error) {
-        
+
     }
 
 });
