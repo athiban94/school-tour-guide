400d5b6eeda7c318a16e5f5acdd330406cf04153
Tengger <hui_ro@126.com>
2018-12-09 00:19:36 -0500

    Profile Page Outline

--- app.js
+++ app.js
@@ -40,5 +40,5 @@ require('./routes/passport')(passport);
 
 app.listen(3001, () => {
   console.log("We've now got a server!");
-  console.log("Your routes will be running on http://localhost:3000");
+  console.log("Your routes will be running on http://localhost:3001");
 });
